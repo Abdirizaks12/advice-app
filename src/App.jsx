fa8562d072d6597e2a5c18fa611dec622b53e2a4
Abdirizak <abdirizaksigat99@gmail.com>
2023-01-01 11:24:49 -0800

    added css to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { useState, useEffect} from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './input.css';
 
 export default function App() {
-  const[advice, setAdvice]= useState({id:"", advice:""})
+  const [advice, setAdvice] = useState({ id: '', advice: '' });
   // useEffect(()=>{
 
   //   fetch("https://api.adviceslip.com/advice")
@@ -10,20 +11,19 @@ export default function App() {
   //     .then(data=> setAdvice(data.slip))
   // },[])
 
-   function handleChange(){
-    fetch("https://api.adviceslip.com/advice")
-      .then(res=> res.json())
-      .then(data=> setAdvice(data.slip))
-   }
+  function handleChange() {
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => res.json())
+      .then((data) => setAdvice(data.slip));
+  }
   return (
+    <div className="main">
+      <h1 className="header"> ADVICE #{advice.id}</h1>
+      <h1 className="advice">"{advice.advice}"</h1>
 
-
-    <div>
-      <h1> ADVICE #{advice.id}</h1>
-      <h1>"{advice.advice}"</h1>
-
-      <button onClick={handleChange}>next advice</button>
-
+      <button className="advice-btn" onClick={handleChange}>
+        next advice
+      </button>
     </div>
-  )
+  );
 }
