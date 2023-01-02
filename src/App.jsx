87225c40868e57c33c6be52a9c110c7ef22adeb1
Abdirizak <abdirizaksigat99@gmail.com>
2023-01-01 21:41:54 -0800

    refactored the code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './input.css';
 
 export default function App() {
   const [advice, setAdvice] = useState({ id: '', advice: '' });
+  const [initial, setInitial] = useState(false);
   // useEffect(()=>{
 
   //   fetch("https://api.adviceslip.com/advice")
@@ -15,15 +16,29 @@ export default function App() {
     fetch('https://api.adviceslip.com/advice')
       .then((res) => res.json())
       .then((data) => setAdvice(data.slip));
+
+    setInitial(true);
   }
   return (
     <div className="main">
-      <h1 className="header"> ADVICE #{advice.id}</h1>
-      <h1 className="advice">"{advice.advice}"</h1>
+      {initial ? (
+        <div>
+          <h1 className="header"> ADVICE #{advice.id}</h1>
+          <h1 className="advice">"{advice.advice}"</h1>
+        </div>
+      ) : (
+        []
+      )}
 
-      <button className="advice-btn" onClick={handleChange}>
-        next advice
-      </button>
+      {initial == false ? (
+        <button className="advice-btn" onClick={handleChange}>
+          Get advice
+        </button>
+      ) : (
+        <button className="advice-btn" onClick={handleChange}>
+          next advice
+        </button>
+      )}
     </div>
   );
 }
